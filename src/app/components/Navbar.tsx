b535ea19bc9eb11a139da0cf71d46c16a1b19012
5hiV2anSh <piyushfulper3210@gmail.com>
2024-03-26 23:47:30 +0530

    First Commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import clsx from "clsx";
+import React, { useState } from "react";
+import { Content, KeyTextField } from "@prismicio/client";
+import Button from "./button";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function NavBar({
+  settings,
+}: {
+  settings: Content.SettingsDocument;
+}) {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  const links = [
+    { label: "About", href: "/about" },
+    { label: "Projects", href: "/projects" },
+    { label: "Blog", href: "/blog" },
+  ];
+
+  return (
+    <nav aria-label="Main navigation">
+      <ul className="flex flex-col justify-between rounded-b-lg bg-slate-50 px-4 py-2 md:m-4 md:flex-row md:items-center md:rounded-xl">
+        <div className="flex items-center justify-between">
+          <NameLogo name={settings.data.name} />
+          <Button
+            linkField={settings.data.cta_link}
+            label={settings.data.cta_label}
+            className="block p-2 text-2xl text-slate-800 md:hidden"
+          />
+        </div>
+        <div
+          className={clsx(
+            "fixed bottom-0 left-0 right-0 top-0 z-50 flex flex-col items-end gap-4 bg-slate-50 pr-4 pt-14 transition-transform duration-300 ease-in-out md:hidden",
+            open ? "translate-x-0" : "translate-x-[100%]",
+          )}
+        >
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+          <li>
+            <Button
+              linkField={settings.data.cta_link}
+              label={settings.data.cta_label}
+              className="ml-3"
+            />
+          </li>
+        </div>
+        <DesktopMenu settings={settings} pathname={pathname} />
+      </ul>
+    </nav>
+  );
+}
+
+function NameLogo({ name }: { name: KeyTextField }) {
+  return (
+    <Link
+      href="/"
+      aria-label="Home page"
+      className="text-xl font-extrabold tracking-tighter text-slate-900"
+    >
+      {name}
+    </Link>
+  );
+}
+
+function DesktopMenu({
+  settings,
+  pathname,
+}: {
+  settings: Content.SettingsDocument;
+  pathname: string;
+}) {
+  return (
+    <div className="relative z-50 hidden flex-row items-center gap-1 bg-transparent py-0 md:flex">
+      <li>
+        <Button
+          linkField={settings.data.cta_link}
+          label={settings.data.cta_label}
+          className="ml-3"
+        />
+      </li>
+    </div>
+  );
+}
---
+++ src/app/components/button.tsx
@@ -0,0 +1,43 @@
+import { PrismicNextLink } from "@prismicio/next";
+import React from "react";
+import { MdArrowOutward } from "react-icons/md";
+import clsx from "clsx";
+import { KeyTextField, LinkField } from "@prismicio/client";
+
+type ButtonProps = {
+  linkField: LinkField;
+  label: KeyTextField;
+  showIcon?: boolean;
+  className?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+};
+
+export default function Button({
+  linkField,
+  label,
+  showIcon = true,
+  className,
+  children,
+  onClick,
+}: ButtonProps) {
+  return (
+    <PrismicNextLink
+      field={linkField}
+      className={clsx(
+        "group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-300 px-4 py-2 font-bold transition-transform ease-out text-black hover:scale-105",
+        className,
+      )}
+    >
+      <span
+        className={clsx(
+          "absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform  duration-300 ease-in-out group-hover:translate-y-0",
+        )}
+      />
+      <span className="relative flex items-center justify-center gap-2">
+        {label} {showIcon && <MdArrowOutward className="inline-block" />}
+        {children}
+      </span>
+    </PrismicNextLink>
+  );
+}
